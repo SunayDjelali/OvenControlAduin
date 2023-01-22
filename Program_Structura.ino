08cd45a21eafa339603644e3b33fa43cb65be9cc
Quti <quti@abv.bg>
2023-01-22 22:12:02 +0200

    New Begin, Proram Stucture, same Seting for Mega Board

--- Program_Structura.ino
+++ Program_Structura.ino
@@ -1,44 +1,69 @@
-//Define variables pins e.t.
-//includes
+//Includes
 #include <arduino.h>
 #include <max6675.h>
 #include <Countimer.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
-// Module Termocople connection pins (Digital Pins)
-int thermoDO = 22;
-int thermoCS = 23;
-int thermoSCK = 24;
-int currentTemperature;
-int choisetTemperature;
+//Define Display Seting
+#define OLED_RESET -1 
+#define SCREEN_ADDRESS 0x3C // OLED display Addres
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 32  // OLED display height, in pixels
 
-//Set pin  for relay in arey
-int RelayPins[] = { 2, 3, 4, 5 };
+//Set pin for relay in Array
+int RelayPins[] = { 22, 23, 24, 25 };
 
-//set pin for button rottery encoder
-int InputPinsButton[] = { 6, 7};
+//Set pin for button rottery encoder in Array (Digital Pins)
+int InputPinsButton[] = { 4, 7};
 
-//Define Rotteri encoder set hronometer
-#define TimerCLK    8
-#define TimerDT     9
-#define TimerSW     InputPinsButton[0]
-int TimercurrentCLK; 
-int TimerpreviousCLK; 
+//Define Rotteri encoder set hronometer (Digital Pins)
+#define TimerCLK    2 //Pin on the boardclockwise detection
+#define TimerDT     3 //Pin on the board counter clockwise detection
+#define TimerSW     InputPinsButton[0] //Pin on the board Switch button
+int CounterTimer = 0; //Variable countering Timer
+int currentTimerCLK;  //Variable cureent countering  value Timer
+int previousTimerCLK; //Variable for previous value
+// ? Variable for choise timer ? what taype ? how many variables ? i don't know at this stage
+int ChoisetTime;
 
-//Define rottery encoder set temperature
-#define TemperatureCLK    10
-#define TemperatureDT     11
-#define TemperatureSW     InputPinsButton[1]
-int TemperaturecurrentCLK; 
-int TemperaturepreviousCLK; 
+//Define rottery encoder set temperature (Digital Pins)
+#define TemperatureCLK    5 //Pin on the board clockwise detection
+#define TemperatureDT     6 //Pin on the board counter clockwise detection
+#define TemperatureSW     InputPinsButton[1] //Pin on the board Switch button
+int CounterTemperature = 0; //Variable countering temperature
+int currentTemperatureCLK;  //Variable cureent countering  value temperature
+int previousTemperatureCLK; //Variable for previous value temperature
+int choisetTemperature;    //Variable for Need temperature
 
-//Code for Termocuple
-MAX6675 thermocouple(thermoSCK, thermoCS, thermoDO);
+//define Pin for the Piezo buzer  
+#define Buzzer 8 // Pin on the board piezobuzer +
+//Increase or decrease setting value ie. step 
+#define step 10 
+
+//define Module Termocople connection pins (Digital Pins)
+#define thermoSO  9 //Pin on the board SO
+#define thermoCS  10 //Pin on the board CS
+#define thermoSCK  11 //Pin on the board SCK
+int currentTemperatureMAX6675;
+//define Text Variables
+String strTemperature = "Temperature:";
+String strTime =        "Time       :";
+
+//Create object MAX6675 chip, K-type Termocuple 
+MAX6675 thermocouple(thermoSCK, thermoCS, thermoSO);
+
+//Create object OLED display, SSD1306 chip
+Adafruit_SSD1306 display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //choise what type of timer use from Countimer Class
 Countimer tDown;
 
 void setup() {
     //Setings Here pins mode OUTPUT, INPUTS
+
     //OUTPUT General ordinary
     for (int i = 0; i < 4; i++) {
       pinMode(RelayPins[i], OUTPUT);
@@ -51,69 +76,152 @@ void setup() {
     }
     // Termocuple set pins Mode INPUT
     pinMode(thermoSCK, INPUT);
-    pinMode(thermoDO,INPUT);
+    pinMode(thermoSO,INPUT);
     pinMode(thermoSCK,INPUT);
     //rottery encoder Timer set pins Mode INPUT
     pinMode(TimerCLK,INPUT);
     pinMode(TimerDT, INPUT);
+    pinMode(TimerSW, INPUT);
     //Rottery Encoder Temperatura set pins Mode INPUT
     pinMode(TemperatureCLK,INPUT);
     pinMode(TemperatureDT, INPUT);
-    TimerpreviousCLK = digitalRead(TimerCLK);
-    TemperaturepreviousCLK = digitalRead(TemperatureCLK);
+    pinMode(TemperatureSW, INPUT);
+
+    //variable = value from pin
+    previousTimerCLK = digitalRead(TimerCLK);
+    previousTemperatureCLK = digitalRead(TemperatureCLK);
 
   delay(500);  // wait for MAX chip to stabilize  
 }
 
 void loop() {
   //here is main loping runining program go in everi functions one by one
-    funcTimer();
-    funcHeating();
-    func3();
-}
+    //funcTimer();
+    //funcHeating();
+    //func3();
+  //Code for Temperature Read parameters from Roterey encoder Temperature
+  if (digitalRead(TemperatureSW) == LOW) {
+    CounterTemperature = 0;
+    choisetTemperature = CounterTemperature;
+    OLED_Dispaly();
+    tone(Buzzer, 300);
+    delay(200);
+    noTone(Buzzer);
+  }
+  //variable = value from pin
+  currentTemperatureCLK = digitalRead(TemperatureCLK);
 
+  if (currentTemperatureCLK != previousTemperatureCLK) {
+    if (digitalRead(TemperatureDT) != currentTemperatureCLK) {
+      CounterTemperature = (CounterTemperature + step);
+      choisetTemperature = CounterTemperature;
+      tone(Buzzer, 600);
+      delay(200);
+      noTone(Buzzer);
+    } else {
+      if (CounterTemperature > 0) {
+        CounterTemperature = (CounterTemperature - step);
+        choisetTemperature = CounterTemperature;
+        tone(Buzzer, 500);
+        delay(200);
+        noTone(Buzzer);
+      } else {
+        CounterTemperature = 0;
+        tone(Buzzer, 1000);
+        delay(1000);
+        noTone(Buzzer);
+      }
+    }
+    OLED_Dispaly();
+  }
+
+  previousTemperatureCLK = currentTemperatureCLK;
+
+ //Code for Timer Read parameters from Roterey encoder Timer 
+
+     if (digitalRead(TimerSW) == LOW) {
+     CounterTimer = 0;
+     choisetTemperature = CounterTimer;
+     OLED_Dispaly();
+     tone(Buzzer, 400);
+     delay(300);
+     noTone(Buzzer);
+   }
+   currentTimerCLK = digitalRead(TimerCLK);
+   if (currentTimerCLK != previousTimerCLK) {
+     if (digitalRead(TimerDT) != currentTimerCLK) {
+       CounterTimer = (CounterTimer + step);
+       ChoisetTime = CounterTimer;
+       tone(Buzzer, 700);
+       delay(300);
+       noTone(Buzzer);
+     } else {
+       if (CounterTimer > 0) {
+         CounterTimer = (CounterTimer - step);
+         ChoisetTime = CounterTimer;
+         tone(Buzzer, 600);
+         delay(300);
+         noTone(Buzzer);
+       } else {
+         CounterTimer = 0;
+         tone(Buzzer, 900);
+         delay(1000);
+         noTone(Buzzer);
+       }
+     }
+     OLED_Dispaly();
+   }
+   previousTimerCLK = currentTimerCLK;
+
+
+
+}
+//At this time i don'tknow work this?
 void funcTimer() {
   //functione when pres furst buttone set time conter down
   tDown.setCounter(00, 10, 00, tDown.COUNT_DOWN , tDownComplete);
   //tDown.run();
   //tDown.start();
 }
+void OLED_Dispaly() {
+
+  display.setTextColor(WHITE);
+  display.clearDisplay();//Clearing Display
+  display.setTextSize(1);//Font Size
+  display.setCursor(0, 0);//Sting posiston cursor
+  display.print(strTemperature); //showing Text
+  display.print(CounterTemperature);// Showing Variable Value
+  display.setCursor(0, 10); //Sting posiston cursor
+  display.print("Variable   :"); //showing Text
+  display.print(choisetTemperature); //Showing Variable Value
+  display.setCursor(0, 20); //Sting posiston cursor
+  display.print(strTime);
+  display.print(ChoisetTime); //Showing Variable Value
+  display.display();
+
+}
 
 void funcHeating() {
   //functione when press second buttone set off teperature
  //Create Variable for temperatore store
-  currentTemperature = (int)thermocouple.readCelsius();
+  currentTemperatureMAX6675 = (int)thermocouple.readCelsius();
 //------------------------------------------------------------------
-  // Degree Mark, length=1, position=3 (right)
+  //Degree Mark, length=1, position=3 (right)
   //display1.showString("\xB0", 1, 3);
   //display2.showString("\xB0", 1, 3);
-  // Number, length=3, position=0 (left)
-  //display1.showNumber(temp, false, 3, 0);
-  //display2.showNumber(temp, false, 3, 0);
-  // Code for display end
   Serial.println(thermocouple.readCelsius());
-  // if temprature goes above 27.0C, turn the relay ON
-
+  // if temprature goes above choiset C, turn the relay ON
     if (thermocouple.readCelsius() > choisetTemperature) {
-      digitalWrite(RelayPins[0], HIGH);  // set pin 10 LOW
+      digitalWrite(RelayPins[0], HIGH);  // set pin 22 LOW
     } else {
-      digitalWrite(RelayPins[0], LOW);  // set pin 10 HIGH
+      digitalWrite(RelayPins[0], LOW);  // set pin 22 HIGH
     }
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-  delay(3000);
+  delay(500);
 
 }
-
-void func3() {
-  //main Functione where the run
-  //read temperature and show on the display
-  //Show set value of timer blinking 
-  //turn on heating by relay to the chosen temperatura
-  //run conterdown timer by triger
-  //when conterdown time is 00:00 heating off
-  delay(400);
-}
+//When time is end 00:00 turn off Oven
 void tDownComplete()
 {
-	digitalWrite(13, HIGH);
+	digitalWrite(RelayPins[0], HIGH);
 }
